dbb7e39c9d86dd94c8eb9f1b9af663ccb7e1d306
Vijay Limbasiya <vijay.limbasiya@netcracker.com>
2019-02-02 11:28:57 +0530

    sfd

--- t3.sh
+++ t3.sh
@@ -2,4 +2,4 @@
 cd ~/pypreprocess && . continuous_integration/install_spm12.sh
 source ~/neurodebian-travis.sh -y
 python3 setup.py install
-cd ~/pypreprocess/examples/easy_start && python3 nipype_preproc_spm_auditory.py
+cd ~/pypreprocess/pypreprocess/examples/easy_start && python3 nipype_preproc_spm_auditory.py
